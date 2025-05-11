ed296afd9e0aa06d3fbbe847ad2f4ae181b23cf7
Sahil Singla <sahilrajiv.singla@gmail.com>
2025-05-12 02:40:17 +0530

    submit

--- pages/mybookings.php
+++ pages/mybookings.php
@@ -5,19 +5,6 @@ include('../base/base_header.php');
 <?php
 $userid = $_SESSION['user']['user_id'];
 $username = $_SESSION['user']['username'];
-
-$filterdate = $_GET['filter'] ?? '';
-
-$datequery = "SELECT DISTINCT YEAR(created_at) as bookingdate FROM `userbooking` WHERE `userid` = $userid AND `status` = 1";
-$dateresult = mysqli_query($conn, $datequery);
-
-$dateCondition = '';
-if ($filterdate) {
-    $dateCondition = "AND YEAR(created_at) = '$filterdate'";
-}
-
-$query = "SELECT * FROM `userbooking` WHERE `userid` = $userid AND `status` = 1 $dateCondition";
-$result = mysqli_query($conn, $query);
 ?>
 
 <div class="bg-secondary bg-opacity-80">
@@ -27,98 +14,80 @@ $result = mysqli_query($conn, $query);
                 <div class="p-4 border-2 rounded-4 shadow-sm bg-light">
                     <div class="d-flex justify-content-between align-items-center mb-3">
                         <h3 class="mb-0">🛏️ Your Booked Rooms</h3>
-                        <form method="GET" class="d-flex align-items-center" style="width: 20%;">
-                            <!-- <label for="filter" class="me-2 fw-semibold" style="width: 100px;">Choose Your Booking Date:</label> -->
-                            <select name="filter" id="filter" class="form-select" onchange="this.form.submit()">
-                                <option value="">All Rooms</option>
-                                <?php while ($daterow = mysqli_fetch_assoc($dateresult)) {
-                                    $date = $daterow['bookingdate'];
-                                ?>
-                                    <option value="<?php echo $date; ?>" <?php if ($filterdate === $date) echo 'selected'; ?>>
-                                        <?php echo $date; ?>
-                                    </option>
-                                <?php
-                                }
-                                ?>
-                            </select>
-                        </form>
                     </div>
 
-                    <?php
-                    $totalprice = 0;
-                    $totalRoomTypes = 0;
-                    $totalRoomCount = 0;
+                    <div class="card shadow-sm border-secondary rounded-4 overflow-hidden h-100 my-4">
+                        <?php
+                        $datequery = "SELECT DISTINCT DATE(created_at) as bookingdate FROM `userbooking` WHERE `userid` = $userid AND `status` = 1";
+                        $dateresult = mysqli_query($conn, $datequery);
 
-                    while ($row = mysqli_fetch_assoc($result)) {
+                        $totalprice = 0;
+                        $totalRoom = 0;
+                        $totalBookings = 0;
 
-                        $quantity = $row['quantity'];
-                        $roomid = $row['roomid'];
-
-                        $bookingdate = date('Y-m-d', strtotime($row['created_at']));
-
-                        $roomquery = "SELECT * FROM `room` WHERE `id` = $roomid";
-                        $roomresult = mysqli_query($conn, $roomquery);
-
-                        $totalRoomTypes++;
-                        $totalRoomCount += $quantity;
-
-                        while ($roomrow = mysqli_fetch_assoc($roomresult)) {
-                            $subt = $quantity * $roomrow['price'];
-                            $totalprice += $subt;
-                    ?>
-                            <div class="card shadow-sm border-secondary rounded-4 overflow-hidden h-100 my-4">
-                                <div class="card-top" style="height: 50px; background: rgba(108, 117, 125, 0.50);">
-                                    <div class="d-flex justify-content-between align-items-center m-3">
-
-                                        <p><strong>Room is Booked <i class="fa-regular fa-circle-check text-success"></i></strong></p>
-                                        <p><strong>Booked By : <?php echo ucfirst($username) ?> </strong></p>
-                                        <p><strong>Booked On: <?php echo $bookingdate ?></strong> </p>
-
-                                    </div>
+                        while ($daterow = mysqli_fetch_assoc($dateresult)) {
+                            $bookingdate = $daterow['bookingdate'];
+                        ?>
+                            <div class="card-top mt-1" style="height: 65px; background: rgba(108, 117, 125, 0.50);">
+                                <div class="d-flex justify-content-between align-items-baseline m-3">
+                                    <p><strong>Previously Reserved Rooms <i class="fa-regular fa-circle-check text-success"></i></strong></p>
+                                    <p><strong>Booked By : <?php echo ucfirst($username) ?> </strong></p>
+                                    <p><strong>Booked On: <?php echo $bookingdate ?></strong> </p>
+                                    <a href="bookingdetail.php?date=<?php echo $bookingdate; ?>" class="btn btn-success ">Booking Details</a>
                                 </div>
+                            </div>
 
-                                <div class="card">
-                                    <div class="row g-0">
-                                        <div class="col-md-5">
-                                            <img src="../public/upload/rooms/<?php echo $roomrow['image'] ?>" class="img-fluid w-100 object-fit-cover" style="height: 200px;" alt="Room Image">
-                                        </div>
+                            <?php
+                            $query = "SELECT * FROM `userbooking` WHERE `userid` = '$userid' AND DATE(`created_at`) = '$bookingdate' AND `status` = 1";
+                            $result = mysqli_query($conn, $query);
 
-                                        <div class="col-md-7">
-                                            <div class="card-body text-dark">
 
-                                                <div class="d-flex justify-content-between align-items-center">
-                                                    <div>
-                                                        <h4 class="card-title fw-bold"><?php echo $roomrow['title'] ?></h4>
-                                                        <p class="text-muted"><?php echo $roomrow['shortdescription'] ?></p>
+                            $totalBookings++;
+
+                            while ($row = mysqli_fetch_assoc($result)) {
+
+                                $quantity = $row['quantity'];
+                                $roomid = $row['roomid'];
+
+                                $roomquery = "SELECT * FROM `room` WHERE `id` = $roomid";
+                                $roomresult = mysqli_query($conn, $roomquery);
+
+                                $totalRoom++;
+
+                                while ($roomrow = mysqli_fetch_assoc($roomresult)) {
+                                    $subt = $quantity * $roomrow['price'];
+                                    $totalprice += $subt;
+                            ?>
+                                    <div class="card mx-3 my-2">
+                                        <div class="row g-0">
+                                            <div class="col-md-5">
+                                                <img src="../public/upload/rooms/<?php echo $roomrow['image'] ?>" class="img-fluid w-100 object-fit-cover" style="height: 150px;" alt="Room Image">
+                                            </div>
+
+                                            <div class="col-md-7">
+                                                <div class="card-body text-dark">
+                                                    <h4 class="card-title fw-bold"><?php echo $roomrow['title'] ?></h4>
+                                                    <p class="text-muted"><?php echo $roomrow['shortdescription'] ?></p>
+                                                    <div class="d-flex justify-content-between align-items-center mt-1">
+                                                        <div class="my-2">
+                                                            <p><strong>Booking Id : <?php echo $row['id'] ?> </strong></p>
+                                                            <p><strong>Booked Price: ₹<?php echo $roomrow['price'] ?></strong></p>
+                                                        </div>
+                                                        <div class="my-2">
+                                                            <p><strong>Booked Quantity: <?php echo $quantity ?></strong></p>
+                                                            <p class="subtotal"><strong>Subtotal Money Spent: ₹<?php echo $subt ?></strong></p>
+                                                        </div>
                                                     </div>
-                                                    <p><strong>Booking Id : <?php echo $row['id'] ?> </strong></p>
-                                                </div>
-
-                                                <div class="d-flex justify-content-between align-items-center mt-2">
-                                                    <p><strong>Booked Price: ₹<?php echo $roomrow['price'] ?></strong></p>
-                                                    <p><strong>Booked Quantity: <?php echo $quantity ?></strong></p>
-                                                </div>
-
-                                                <p class="subtotal"><strong>Subtotal Money Spent: ₹<?php echo $subt ?></strong></p>
-
-                                                <div class="d-flex justify-content-between align-items-center mt-3">
-                                                    <a href="bookingdetail.php?id=<?php echo $roomrow['id']; ?>&date=<?php echo $bookingdate; ?>" class="btn btn-outline-primary">Booking Details</a>
-                                                    <a href="roomdetail.php?id=<?php echo $roomrow['id'] ?>" class="btn btn-outline-success">Book Again</a>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-
-                    <?php
+                        <?php
+                                }
+                            }
                         }
-                    }
-
-                    if ($totalRoomTypes == 0) {
-                        echo "<p class='text-center text-muted'>No bookings found for the selected time period.</p>";
-                    }
-                    ?>
+                        ?>
+                    </div>
                 </div>
             </div>
 
@@ -127,14 +96,15 @@ $result = mysqli_query($conn, $query);
                     <div class="p-4 border-2 rounded-4 shadow bg-light text-center">
                         <h4 class="mb-3">📊 Booking Summary</h4>
                         <p class="fs-5"><strong>Total Money Spent:</strong> ₹<?php echo $totalprice; ?></p>
-                        <p><strong>Room Types:</strong> <?php echo $totalRoomTypes; ?></p>
-                        <p><strong>Total Rooms Booked:</strong> <?php echo $totalRoomCount; ?></p>
+                        <p><strong>Total Booked Rooms:</strong> <?php echo $totalRoom; ?></p>
+                        <p><strong>Total Number of Bookings:</strong> <?php echo $totalBookings; ?></p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 
 <?php include('./exploring.php'); ?>
 <?php include('../base/base_footer.php'); ?>
